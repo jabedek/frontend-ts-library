682411f473ad9cfa6199b7e709e5384c8f840a2e
jabedek <jabedek@gmail.com>
2023-04-08 21:34:02 +0200

    'next'

--- dist/utils/standalone/input-elements.js
+++ dist/utils/standalone/input-elements.js
@@ -1,7 +1,7 @@
 import "../../global-extensions";
 /** Distinct `id` should be provided for HTML (or custom) input if we want to combine it with HTML label and other browser functionalities. */
-export function createInputId(inputType) {
-    return `input-${inputType}-${Math.randomInt(1000, 9000)}-${Math.randomInt(1000, 9000)}`
+export function createInputId(dataName, inputType) {
+    return `${dataName}-input-${inputType}-${Math.randomInt(1000, 9000)}-${Math.randomInt(1000, 9000)}`
         .replace(/\-/gm, "_")
         .replace(/(\_{2,})/gm, "_");
 }
--- src/utils/standalone/input-elements.ts
+++ src/utils/standalone/input-elements.ts
@@ -1,11 +1,25 @@
 import "../../global-extensions";
 
+type InputDateTime = "date" | "month" | "time" | "week";
+type InputChoice = "checkbox" | "radio" | "select";
+type InputWrite =
+  | "text"
+  | "email"
+  | "password"
+  | "number"
+  | "search"
+  | "url"
+  | "tel"
+  | "textarea";
+
+type InputType = InputDateTime | InputChoice | InputWrite;
+
 /** Distinct `id` should be provided for HTML (or custom) input if we want to combine it with HTML label and other browser functionalities. */
-export function createInputId(inputType: string): string {
-  return `input-${inputType}-${Math.randomInt(1000, 9000)}-${Math.randomInt(
+export function createInputId(dataName: string, inputType: InputType): string {
+  return `${dataName}-input-${inputType}-${Math.randomInt(
     1000,
     9000
-  )}`
+  )}-${Math.randomInt(1000, 9000)}`
     .replace(/\-/gm, "_")
     .replace(/(\_{2,})/gm, "_");
 }
