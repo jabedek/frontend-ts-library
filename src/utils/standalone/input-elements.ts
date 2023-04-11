69cb1696d991d828f84dcd14dc2ffc649489d423
jabedek <jabedek@gmail.com>
2023-04-12 00:04:36 +0200

    'next'

--- src/utils/standalone/input-elements.ts
+++ src/utils/standalone/input-elements.ts
@@ -16,7 +16,7 @@ type InputType = InputDateTime | InputChoice | InputWrite;
 
 /** Distinct `id` should be provided for HTML (or custom) input if we want to combine it with HTML label and other browser functionalities. */
 export function createInputId(dataName: string, inputType: InputType): string {
-  return `${dataName}-input-${inputType}-${Math.randomInt(
+  return `@id-${dataName}-input-${inputType}-${Math.randomInt(
     1000,
     9000
   )}-${Math.randomInt(1000, 9000)}`
