d136abed54825f5e89cc81b0e3fb427a71d704a5
jabedek <jabedek@gmail.com>
2023-05-23 07:04:48 +0200

    'next'

--- dist/global-extensions.js
+++ dist/global-extensions.js
@@ -3,12 +3,12 @@ import { default as arrayUtilsProtected } from "./utils/for-extensions/protected
 import { default as promiseUtilsProtected } from "./utils/for-extensions/protected/promise";
 const logs = [];
 function logExtensionsAdded() {
-    const emojiStyle = "background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px; font-weight: 600; height: 20px;";
-    const nameStyle = "color: lightgreen; background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px 3.2px; font-weight: 600; height: 20px;";
-    const nameStyle2 = "color: lightorange; background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px 3.2px; font-weight: 600; height: 20px;";
-    const defaultStyle = "color: rgba(200,200,230,1); background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px 3.2px; height: 20px;";
+    const emojiStyle = "background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px; font-weight: 600; height: 20px;";
+    const nameStyle = "color: lightgreen; background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px 1px; font-weight: 600; height: 20px;";
+    const nameStyle2 = "color: lightsalmon; background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px 1px; font-weight: 600; height: 20px;";
+    const defaultStyle = "color: rgba(200,200,230,1); background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px; height: 20px;";
     console.groupCollapsed("New functions successfully added to your project:");
-    logs.forEach(({ fnName, objName }) => console.log(`%c🎉%c${objName}[%c${fnName}]%cwas set as new property to%c🎉`, emojiStyle, nameStyle2, nameStyle, defaultStyle, emojiStyle));
+    logs.forEach(({ fnName, objName }) => console.log(`%c🎉%cadded %c${objName}%c[${fnName}]%c🎉`, emojiStyle, defaultStyle, nameStyle2, nameStyle, emojiStyle));
     console.log(`&cThat's only those added to global objects. Check out what else is new at: https://www.npmjs.com/package/frotsi`, defaultStyle);
     console.groupEnd();
 }
--- src/global-extensions.ts
+++ src/global-extensions.ts
@@ -100,25 +100,25 @@ const logs: { objName: string; fnName: string }[] = [];
 
 function logExtensionsAdded() {
   const emojiStyle =
-    "background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px; font-weight: 600; height: 20px;";
+    "background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px; font-weight: 600; height: 20px;";
 
   const nameStyle =
-    "color: lightgreen; background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px 3.2px; font-weight: 600; height: 20px;";
+    "color: lightgreen; background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px 1px; font-weight: 600; height: 20px;";
 
   const nameStyle2 =
-    "color: lightorange; background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px 3.2px; font-weight: 600; height: 20px;";
+    "color: lightsalmon; background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px 1px; font-weight: 600; height: 20px;";
 
   const defaultStyle =
-    "color: rgba(200,200,230,1); background: rgba(10,0,0,0.5); font-size: 11px; padding: 6px 3.2px; height: 20px;";
+    "color: rgba(200,200,230,1); background: rgba(10,0,0,0.5); font-size: 12px; text-shadow:1px 1px 2px rgba(255,255,0,0.75); padding: 6px; height: 20px;";
 
   console.groupCollapsed("New functions successfully added to your project:");
   logs.forEach(({ fnName, objName }) =>
     console.log(
-      `%c🎉%c${objName}[%c${fnName}]%cwas set as new property to%c🎉`,
+      `%c🎉%cadded %c${objName}%c[${fnName}]%c🎉`,
       emojiStyle,
+      defaultStyle,
       nameStyle2,
       nameStyle,
-      defaultStyle,
       emojiStyle
     )
   );
