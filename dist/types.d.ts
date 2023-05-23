2e08754ff1d4c9c12f03f291472a206391dfb647
jabedek <jabedek@gmail.com>
2023-05-23 16:13:41 +0200

    'next'

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -7,6 +7,15 @@ export declare type Flatten<T> = {
     [K in keyof T]: T[K];
 } & {};
 /**
+ * Does what `Flatten` sometimes can't in more complicated and nested object types.
+ * `Flatten` takes a type T and tries to flatten its type hierarchy by combining all of its properties into a single, non-nested type.
+ *
+ * @see https://www.youtube.com/watch?v=2lCCKiWGlC0
+ */
+export declare type DeepFlatten<T> = {
+    [K in keyof T]: Flatten<T[K]>;
+} & {};
+/**
  * Represents a callback function provided as argument to another one. Takes 2 arguments - `A` & `R` - for parameters and return types respectively.
  */
 export declare type CallbackFn<P extends Params = unknown[], R extends any = void> = (...args: P) => R;
--- src/types.ts
+++ src/types.ts
@@ -8,6 +8,16 @@ export type Flatten<T> = {
 } & {};
 
 /**
+ * Does what `Flatten` sometimes can't in more complicated and nested object types.
+ * `Flatten` takes a type T and tries to flatten its type hierarchy by combining all of its properties into a single, non-nested type.
+ *
+ * @see https://www.youtube.com/watch?v=2lCCKiWGlC0
+ */
+export type DeepFlatten<T> = {
+  [K in keyof T]: Flatten<T[K]>;
+} & {};
+
+/**
  * Represents a callback function provided as argument to another one. Takes 2 arguments - `A` & `R` - for parameters and return types respectively.
  */
 export type CallbackFn<P extends Params = unknown[], R extends any = void> = (
